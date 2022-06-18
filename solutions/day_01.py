4b4e75b7dc47259c358771b71067086aade0a1db
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-18 22:33:31 +1000

    Setup file and folder templates

---
+++ solutions/day_01.py
@@ -0,0 +1,22 @@
+def main():
+    input = process_input_file()
+    p1_solution = solve_part1(input)
+    print("P1 solution - {}".format(p1_solution))
+    p2_solution = solve_part2(input)
+    print("P2 solution - {}".format(p2_solution))
+
+
+def process_input_file():
+    ()
+
+
+def solve_part1(input):
+    ()
+
+
+def solve_part2(input):
+    ()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/solutions_test_methods.py
@@ -0,0 +1,12 @@
+import unittest
+
+import solutions
+
+
+class SolutionsTestMethods(unittest.TestCase):
+
+    ()
+
+
+if __name__ == "__main__":
+    unittest.main()
