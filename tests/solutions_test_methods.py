71e8da5787e33f6a06332922c818458cca9ad103
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-25 17:34:08 +1000

    Solve Day 1 Part 1

--- tests/solutions_test_methods.py
+++ tests/solutions_test_methods.py
@@ -13,22 +13,22 @@ class SolutionsTestMethods(unittest.TestCase):
     Contains the test methods for each problem part in AOC 2016, checking valid
     answers.
     """
-    
-    # def test_day_01_p1(self):
-    #     """
-    #     Solution test method for AOC 2016 Day 1 Part 1.
-    #     """
-    #     input_data = solutions.day_01.process_input_file()
-    #     solution = solutions.day_01.solve_part1(input_data)
-    #     # self.assertEquals(False, solution)
-    
+
+    def test_day_01_p1(self):
+        """
+        Solution test method for AOC 2016 Day 1 Part 1.
+        """
+        input_data = solutions.day_01.process_input_file()
+        solution = solutions.day_01.solve_part1(input_data)
+        self.assertEqual(332, solution)
+
     # def test_day_01_p2(self):
     #     """
     #     Solution test method for AOC 2016 Day 1 Part 2.
     #     """
     #     input_data = solutions.day_01.process_input_file()
     #     solution = solutions.day_01.solve_part2(input_data)
-    #     # self.assertEquals(False, solution)
+    #     # self.assertEqual(False, solution)
 
 
 
