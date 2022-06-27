fa2ba3cef9b9927dc12b669f00bf5a4e074c4581
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-27 20:00:33 +1000

    Solve Day 5 Part 2

--- src/solutions/day_05.py
+++ src/solutions/day_05.py
@@ -37,4 +37,26 @@ def solve_part1(input_data):
 
 
 def solve_part2(input_data):
-    return -1
+    """
+    Determines the eight-character password for the second door by extracting
+    seventh character from successive "interesting" MD5 hashes where the
+    sixth character represents a valid index.
+    """
+    index = 0
+    door_id = input_data
+    door_password = [" " for _ in range(0, 8)]
+    valid_index = ["0", "1", "2", "3", "4", "5", "6", "7"]
+    password_digits_found = 0
+    # Conduct MD5 hashes to look for "interesting" hashes until password done
+    while password_digits_found < 8:
+        hash_result = hashlib.md5(f"{door_id}{index}".encode())
+        index += 1
+        hex_digest = hash_result.hexdigest()
+        # Check for "interesting" hash with valid index at position 5
+        if hex_digest.startswith("00000") and hex_digest[5] in valid_index:
+            password_index = int(hex_digest[5])
+            if door_password[password_index] != " ":
+                continue
+            door_password[password_index] = hex_digest[6]
+            password_digits_found += 1
+    return "".join(door_password)
