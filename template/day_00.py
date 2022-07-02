81a918f5fffa55671ef943f457f58f7a16d5fb50
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-07-02 14:48:28 +1000

    Update solution template file

---
+++ template/day_00.py
@@ -0,0 +1,25 @@
+"""
+Solutions for AOC 2016 Day ##.
+"""
+
+
+def process_input_file():
+    """
+    Processes the AOC 2016 Day ## input file into the format required by the
+    solver functions.
+    """
+    return -1
+
+
+def solve_part1(input_data):
+    """
+    Solves AOC 2016 Day ## Part 1. ###
+    """
+    return input_data
+
+
+def solve_part2(input_data):
+    """
+    Solves AOC 2016 Day ## Part 2. ###
+    """
+    return input_data
